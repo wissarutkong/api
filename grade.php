c98374660a8c086cb4118652b63182dc255bfb89
wissarutfirst <59951365+wissarutkong@users.noreply.github.com>
2024-08-16 16:59:08 +0700

    Add files via upload

--- grade.php
+++ grade.php
@@ -2,9 +2,6 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    exit(0); // สำหรับ Preflight requests
-}
 
 $score = $_GET['score'];
 $result = array();
